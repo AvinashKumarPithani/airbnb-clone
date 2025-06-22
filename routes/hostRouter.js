5c4a66a225f3dfc98443f3e5534d5319a3a2027d
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-06-22 21:49:33 +0530

    added pathUtil

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const hostRouter = express.Router();
 const path = require("path");
+const rootDir = require("../utils/pathUtil");
 
 hostRouter.get("/add-home", (req, res, next) => {
   console.log("Third Middleware", req.url, req.method);
-  res.status(202).sendFile(path.join(__dirname, '../','views', 'addHome.html'));
+  res.status(202).sendFile(path.join(rootDir,'views', 'addHome.html'));
 });
 hostRouter.post("/add-home", (req, res, next) => {
   console.log("Fourth Middleware", req.url, req.method, req.body);
-  res.status(203).sendFile(path.join(__dirname, '../','views', 'homeAdded.html'));
+  res.status(203).sendFile(path.join(rootDir,'views', 'homeAdded.html'));
 });
 
 module.exports = hostRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const userRouter = express.Router();
 const path = require("path");
+const rootDir = require("../utils/pathUtil");
 
 userRouter.get("/", (req, res, next) => {
   console.log("Second Middleware", req.url, req.method);
-  res.status(201).sendFile(path.join(__dirname, '../','views', 'home.html'));
+  res.status(201).sendFile(path.join(rootDir,'views', 'home.html'));
 });
 
 module.exports = userRouter;
