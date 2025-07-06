4e560ee54c102b37dc5968067f11a307665cab92
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-06 17:34:59 +0530

    added styling to all pages

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const rootDir = require("./utils/pathUtil");
 
 const app = express();
 
+app.use(express.static(path.join(rootDir, 'public')));
+
 app.use((req, res, next) => {
   console.log("First middleware", req.url, req.method);
   next();
@@ -15,8 +17,6 @@ app.use(express.urlencoded());
 
 app.use(userRouter);
 app.use("/host", hostRouter);
-
-app.use(express.static(path.join(rootDir, 'public')));
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
